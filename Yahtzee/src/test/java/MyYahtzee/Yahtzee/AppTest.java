0b5e20aa78ebde490edff021eed1366f4c20f712
sh-anousheh <sh-anousheh@users.noreply.github.com>
2019-09-20 17:30:57 -0400

    - Create AppTest class and add testReturnIntFor method

---
+++ Yahtzee/src/test/java/MyYahtzee/Yahtzee/AppTest.java
@@ -0,0 +1,22 @@
+package MyYahtzee.Yahtzee;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import junit.framework.TestCase;
+
+public class AppTest extends TestCase {
+
+	private App app;
+
+	public AppTest() {
+		app = new App();
+	}
+
+	public void testReturnIntFor() {
+
+		int res = app.returnIntFor("Write input between 1 and 6", 6);
+
+		assertTrue(res > 0 && res <= 6);
+	}
+}
