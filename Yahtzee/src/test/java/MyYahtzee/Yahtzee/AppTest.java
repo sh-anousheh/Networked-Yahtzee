6e2ebc2eb28b2e048a8dfa53996ade2553994d7b
sh-anousheh <sh-anousheh@users.noreply.github.com>
2019-09-20 17:49:33 -0400

    - Write testHoldSomeDice method in AppTest class

--- Yahtzee/src/test/java/MyYahtzee/Yahtzee/AppTest.java
+++ Yahtzee/src/test/java/MyYahtzee/Yahtzee/AppTest.java
@@ -19,4 +19,60 @@ public class AppTest extends TestCase {
 
 		assertTrue(res > 0 && res <= 6);
 	}*/
+	
+	public void testHoldSomeDice() {
+
+		Random rand = new Random();
+
+		List<Object> randomNumbers = rand.ints(1, 6).distinct().limit(4).boxed().collect(Collectors.toList());
+
+		forHoldSomeDice(randomNumbers);
+	}
+
+	public void forHoldSomeDice(List<Object> holds) {
+
+		String input = "";
+
+		for (Object o : holds) {
+
+			input += o.toString() + " ";
+		}
+
+		Rules game = new Rules();
+
+		int[] dice = game.rolling(5);
+
+		int[] newDice = app.holdSomeDice(input, dice, game);
+
+		boolean pass = true;
+
+		for (int i = 0; i < 5; i++) {
+
+			System.out.print(dice[i]);
+		}
+
+		System.out.println();
+
+		for (int i = 0; i < 5; i++) {
+
+			System.out.print(newDice[i]);
+		}
+
+		System.out.println(holds.toString());
+
+		int i = 0;
+
+		for (Object o : holds) {
+
+			if (newDice[i] != dice[Integer.parseInt(o.toString()) - 1]) {
+				pass = false;
+				break;
+			}
+
+			i++;
+		}
+
+		assertEquals(true, pass);
+	}
+
 }
