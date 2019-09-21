1ead3225b4d7f109b8d06431c085f5d55624e241
sh-anousheh <sh-anousheh@users.noreply.github.com>
2019-09-21 06:52:06 -0400

    - Create TestSuite class

---
+++ Yahtzee/src/test/java/MyYahtzee/Yahtzee/TestSuite.java
@@ -0,0 +1,10 @@
+package MyYahtzee.Yahtzee;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ AppTest.class, RulesTest.class })
+
+public class TestSuite {
+}
