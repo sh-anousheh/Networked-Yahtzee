bffe2ddcad60e8e9f99657910896b100af716239
sh-anousheh <sh-anousheh@users.noreply.github.com>
2019-10-07 15:25:06 -0400

    - Organized test functions

--- Yahtzee/src/main/java/Scenarios/RulesSc.java
+++ Yahtzee/src/main/java/Scenarios/RulesSc.java
@@ -1,6 +1,5 @@
 package Scenarios;
 
-import java.util.*;
 import java.util.Map;
 
 import MyYahtzee.Yahtzee.Rules;
@@ -26,25 +25,41 @@ public class RulesSc {
 
 		ruleClass = new Rules();
 
-	}
-
-	// __________________________________________________________________________
-
-	@Given("Define default answer")
-	public void define_default_answer() {
-
 		pass = true;
 
 	}
 
-	@When("Roll a Die")
+	@Given("Have the Dice with numbers  {int}, {int}, {int}, {int}, {int}")
+	public void have_the_Dice_with_numbers(Integer int1, Integer int2, Integer int3, Integer int4, Integer int5) {
+
+		dice = new int[] { int1, int2, int3, int4, int5 };
+
+	}
+
+	@Then("Verify if the result for {string} is correct")
+	public void verify_if_the_result_for_is_correct(String string) {
+
+		if (pass) {
+
+			System.out.println("Test for " + string + " Passed!");
+
+		} else {
+
+			System.out.println("Test for " + string + " did not Pass!");
+
+		}
+	}
+
+	// __________________________________________________________________________
+
+	@Given("Roll a Die")
 	public void roll_a_Die() {
 
 		dice = ruleClass.rolling(1);
 
 	}
 
-	@Then("Check if the result is between {int} and {int}")
+	@When("Check if the result is between {int} and {int}")
 	public void check_if_the_result_is_between_and(Integer int1, Integer int2) {
 
 		if (!(dice[0] <= int2 && dice[0] >= int1)) {
@@ -53,28 +68,9 @@ public class RulesSc {
 
 		}
 
-		if (pass) {
-
-			System.out.println("Test for 'Roll a Die' Passed!");
-
-		} else {
-
-			System.out.println("Test for 'Roll a Die' did not Pass!");
-
-		}
-
 	}
 
 	// __________________________________________________________________________
-
-	@Given("Roll the Dice")
-	public void roll_the_Dice() {
-
-		dice = new int[] { 3, 2, 6, 6, 2 };
-
-		pass = true;
-
-	}
 
 	@When("Calculate count of numbers")
 	public void calculate_count_of_numbers() {
@@ -102,29 +98,7 @@ public class RulesSc {
 
 	}
 
-	@Then("I verify if the result is correct")
-	public void i_verify_if_the_result_is_correct() {
-
-		if (pass) {
-
-			System.out.println("Test for 'countDice' Passed!");
-
-		} else {
-
-			System.out.println("Test for 'countDice' did not Pass!");
-
-		}
-
-	}
-
 	// __________________________________________________________________________
-
-	@Given("Have the Dice with numbers  {int}, {int}, {int}, {int}, {int}")
-	public void have_the_Dice_with_numbers(Integer int1, Integer int2, Integer int3, Integer int4, Integer int5) {
-
-		dice = new int[] { int1, int2, int3, int4, int5 };
-
-	}
 
 	@When("Choose the category number {int} to score")
 	public void choose_the_category_number_to_score(Integer int1) {
@@ -134,37 +108,50 @@ public class RulesSc {
 		switch (int1) {
 
 		case 1:
+
 			category = box.Aces.name();
+
 			break;
+
 		case 2:
+
 			category = box.Twos.name();
+
 			break;
+
 		case 3:
+
 			category = box.Threes.name();
+
 			break;
+
 		case 4:
+
 			category = box.Fours.name();
+
 			break;
+
 		case 5:
+
 			category = box.Fives.name();
+
 			break;
+
 		default:
+
 			category = box.Sixes.name();
+
 			break;
 		}
 
 	}
 
-	@Then("Check if the results is {int}")
+	@When("Check if the results is {int}")
 	public void check_if_the_results_is(Integer int1) {
 
-		if (ruleClass.getFinalDic().get(category) == int1) {
+		if (ruleClass.getFinalDic().get(category) != int1) {
 
-			System.out.println("Test for " + category + " passed!");
-
-		} else {
-
-			System.out.println("Test for " + category + " did not pass!");
+			pass = false;
 
 		}
 	}
--- Yahtzee/src/test/java/MyYahtzee/Yahtzee/Rules.feature
+++ Yahtzee/src/test/java/MyYahtzee/Yahtzee/Rules.feature
@@ -7,56 +7,64 @@ Feature: Tests for Rules class
   #__________________________________________________________________________
   @tag1
   Scenario: Test for rolling function
-    Given Define default answer
-    When Roll a Die
-    Then Check if the result is between 1 and 6
+    Given Roll a Die
+    When Check if the result is between 1 and 6
+    Then Verify if the result for "rolling function" is correct
 
   #__________________________________________________________________________
   @tag2
   Scenario: Test for countDice function
-    Given Roll the Dice
+    Given Have the Dice with numbers  3, 2, 6, 6, 2
     When Calculate count of numbers
     And Check with the result with 0, 2, 1, 0, 0, 2
-    Then I verify if the result is correct
+    Then Verify if the result for "countDice function" is correct
 
   #__________________________________________________________________________
   @tag3
   Scenario: Test for ones function
     Given Have the Dice with numbers  1, 4, 6, 1, 1
     When Choose the category number 1 to score
-    Then Check if the results is 3
+    And Check if the results is 3
+    Then Verify if the result for "ones function" is correct
 
   #__________________________________________________________________________
   @tag4
   Scenario: Test for twos function
     Given Have the Dice with numbers  1, 2, 6, 4, 1
     When Choose the category number 2 to score
-    Then Check if the results is 2
+    And Check if the results is 2
+    Then Verify if the result for "twos function" is correct
 
   #__________________________________________________________________________
   @tag5
   Scenario: Test for threes function
     Given Have the Dice with numbers  3, 2, 3, 3, 3
     When Choose the category number 3 to score
-    Then Check if the results is 12
+    And Check if the results is 12
+    Then Verify if the result for "threes function" is correct
 
   #__________________________________________________________________________
   @tag6
   Scenario: Test for fours function
     Given Have the Dice with numbers  4, 2, 4, 3, 3
     When Choose the category number 4 to score
-    Then Check if the results is 8
+    And Check if the results is 8
+    Then Verify if the result for "fours function" is correct
 
   #__________________________________________________________________________
   @tag7
   Scenario: Test for fives function
     Given Have the Dice with numbers  4, 5, 5, 5, 3
     When Choose the category number 5 to score
-    Then Check if the results is 15
+    And Check if the results is 15
+    Then Verify if the result for "fives function" is correct
 
   #__________________________________________________________________________
   @tag8
   Scenario: Test for sixes function
     Given Have the Dice with numbers  6, 5, 5, 6, 3
     When Choose the category number 6 to score
-    Then Check if the results is 12
+    And Check if the results is 12
+    Then Verify if the result for "sixes function" is correct
+
+  #__________________________________________________________________________
